6362b3e19f11ce9ddf32391f74cf8433a043896c
GomCoder <k2001911@hanmail.net>
2023-03-03 09:19:42 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import "../style.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
-
